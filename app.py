859ded6f9e35429d809c38343ab7c50f08953542
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-13 11:21:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 from data_funcao import hora_hoje, data_hoje, data_final_abertura, dia_da_semana_extenso
 from api_funcoes import proxima_pagina, todos_eventos, id_sessao_deliberativa, pautas_sessao_deliberativa, mensagem_telegram, mensagem_telegram_2
+from processa_dados_mensagens import processa_update
 from descadrastamento import processo_de_descadrastamento
 
 
@@ -47,9 +48,8 @@ def index():
 @app.route("/bot-aurora-telegram", methods=["POST"]) # método utilizado para enviar dados para o servidor
 def telegram_bot():
   update = request.json
-  processa_dados_mensagens(update)
-  print(resposta.text)
-  return "ok"
+  processa_update()
+  return f'{processa_update()}'
   
   
 @app.route("/bot-aurora-telegram-envio")
