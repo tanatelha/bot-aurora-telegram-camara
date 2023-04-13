0be9e518ae94dd85754dd90342578b60bc7e2c43
Natália Santos <89846568+tanatelha@users.noreply.github.com>
2023-04-12 21:59:31 -0300

    Update processa_dados_mensagens.py

--- processa_dados_mensagens.py
+++ processa_dados_mensagens.py
@@ -1,14 +1,40 @@
-# bibliotecas
+# importar bibliotecas que já vêm com python
+import os # biblioteca para ver chaves em ambiente virtual
 
 
-
-
-
-
+# importar bibliotecas externas: import em ordem alfética e depois froms em ordem alfabética
+import gspread
+import pytz
+import requests
+from flask import Flask, request
+from bs4 import BeautifulSoup
+from datetime import date, datetime, time, timedelta
+from oauth2client.service_account import ServiceAccountCredentials 
 
 from data_funcao import data_hoje, data_final_abertura, dia_da_semana_extenso
 from api_funcoes import proxima_pagina, todos_eventos, id_sessao_deliberativa, pautas_sessao_deliberativa, mensagem_telegram
 from descadrastamento import processo_de_descadrastamento
+
+
+# variáveis de ambiente
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_ADMIN_ID = os.environ["TELEGRAM_ADMIN_ID"]
+
+GOOGLE_SHEETS_KEY = os.environ["GOOGLE_SHEETS_KEY"] 
+
+GOOGLE_SHEETS_CREDENTIALS = os.environ['GOOGLE_SHEETS_CREDENTIALS']
+with open("credenciais.json", mode="w") as arquivo:
+    arquivo.write(GOOGLE_SHEETS_CREDENTIALS)
+conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
+
+api = gspread.authorize(conta)
+planilha = api.open_by_key(f'{GOOGLE_SHEETS_KEY}') 
+sheet_mensagens = planilha.worksheet('mensagens')
+sheet_enviadas = planilha.worksheet('enviadas')
+sheet_inscritos = planilha.worksheet('inscritos')
+sheet_descadastrados = planilha.worksheet('descadastrados')
+
+
 
 
 
@@ -78,7 +104,7 @@ def processa_update(dados):
     texto_resposta = f'Olá, humane! \n \nVocê já se inscreveu para receber os destaques do Executivo publicados no <i>Diário Oficial da União</i>. Agora é só esperar os envios das mensagens todo dia de manhã a partir das 7h \U0001F609 \n \nCaso queira acessar um comando específico, clique em "menu" aqui do lado esquerdo da tela'
 
   nova_mensagem = {"chat_id": chat_id, "text": texto_resposta, "parse_mode": 'html'}
-  requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data = nova_mensagem)
+  resposta = requests.post(f"https://api.telegram.org/bot{token}/sendMessage", data = nova_mensagem)
 
   sheet_inscritos.append_rows(inscricoes)
   sheet_mensagens.append_rows(mensagens)
